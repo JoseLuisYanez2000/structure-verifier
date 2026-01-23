ee9a6aaf1e002692234809a6228f4d991949fd5b
JoseLuisYanez2000 <joseluisyanezmacias.2000@gmail.com>
2026-01-22 23:01:53 -0600

    fix, defaultvalue

--- src/src/verifiers/verifier.ts
+++ src/src/verifiers/verifier.ts
@@ -1,44 +1,53 @@
 import { VerifierConfig } from "../config/verifierConfig";
 import { VerificationError } from "../error/v_error";
 import { VVCIsRequired } from "../interfaces/types";
-import { getMessage, IMessageLanguage, MessageType } from "../languages/message";
+import {
+  getMessage,
+  IMessageLanguage,
+  MessageType,
+} from "../languages/message";
 
 export abstract class Verifier<T> {
-    constructor(protected cond?: VVCIsRequired) { }
-    abstract check(data: any): T;
-    protected badTypeMessage: IMessageLanguage<void>
-    protected isRequired(data: any, isRequired?: boolean): T {
-        let mReq: IMessageLanguage<void> = {
-            es: () => "es requerido y",
-            en: () => "is required and"
-        }
-        if (typeof data === 'string' && this.cond?.emptyAsNull === true) {
-            if (data.length === 0) {
-                data = null;
-            }
-        }
-        let reqVal: MessageType<boolean, void> = false;
-        if (this.cond?.isRequired !== undefined) {
-            reqVal = this.cond.isRequired;
-            if (isRequired !== undefined) {
-                if (typeof reqVal === 'boolean') {
-                    reqVal = isRequired;
-                } else {
-                    reqVal.val = isRequired;
-                }
-            }
-        } else {
-            if (isRequired !== undefined) {
-                reqVal = isRequired;
-            }
-        }
-        if (isRequired && (data === null || data === undefined)) {
-            throw new VerificationError([{
-                key: "",
-                message: `${getMessage(reqVal, undefined, mReq)} ${this.badTypeMessage[VerifierConfig.lang]()}`,
-                isEmpty: true
-            }])
-        }
-        return data as T;
+  constructor(protected cond?: VVCIsRequired) {}
+  abstract check(data: any): T;
+  protected badTypeMessage: IMessageLanguage<void>;
+  protected isRequired(data: any, isRequired?: boolean, defaultValue?: T): T {
+    let mReq: IMessageLanguage<void> = {
+      es: () => "es requerido y",
+      en: () => "is required and",
+    };
+    if (data === undefined || (data === null && defaultValue !== undefined)) {
+      data = defaultValue;
     }
+    if (typeof data === "string" && this.cond?.emptyAsNull === true) {
+      if (data.length === 0) {
+        data = null;
+      }
+    }
+    let reqVal: MessageType<boolean, void> = false;
+    if (this.cond?.isRequired !== undefined) {
+      reqVal = this.cond.isRequired;
+      if (isRequired !== undefined) {
+        if (typeof reqVal === "boolean") {
+          reqVal = isRequired;
+        } else {
+          reqVal.val = isRequired;
+        }
+      }
+    } else {
+      if (isRequired !== undefined) {
+        reqVal = isRequired;
+      }
+    }
+    if (isRequired && (data === null || data === undefined)) {
+      throw new VerificationError([
+        {
+          key: "",
+          message: `${getMessage(reqVal, undefined, mReq)} ${this.badTypeMessage[VerifierConfig.lang]()}`,
+          isEmpty: true,
+        },
+      ]);
+    }
+    return data as T;
+  }
 }
